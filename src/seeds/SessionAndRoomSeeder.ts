64103c34082c26e722748178191a51c1e1a5b5bf
Matheus Honorio <matheushonorio@eng.ci.ufpb.br>
2022-12-10 11:15:07 -0300

    fix: add object array for create rooms with sessions

--- src/seeds/SessionAndRoomSeeder.ts
+++ src/seeds/SessionAndRoomSeeder.ts
@@ -10,31 +10,39 @@ export default class SessionAndRoomSeeder implements Seeder {
     factoryManager: SeederFactoryManager,
   ): Promise<any> {
     const roomRepository = dataSource.getRepository(Room);
+    const sessionRepository = dataSource.getRepository(Session);
 
-    const roomData = {
-      number: 4,
-      capacity: 80,
-    };
+    // Rooms will be created with sessions
+    const roomData = [
+      {
+        number: 5,
+        capacity: 80,
+      },
+      {
+        number: 6,
+        capacity: 80,
+      },
+    ];
 
-    const roomExists = await roomRepository.findOneBy({
-      number: roomData.number,
-    });
+    for (const room of roomData) {
+      const roomExists = await roomRepository.findOneBy({
+        number: room.number,
+      });
 
-    if (!roomExists) {
-      const newRoom = roomRepository.create(roomData);
-      await roomRepository.save(newRoom);
+      if (!roomExists) {
+        const newRoom = roomRepository.create(room);
+        await roomRepository.save(newRoom);
 
-      const sessionRepository = dataSource.getRepository(Session);
+        const sessionData = {
+          timetable: '14:00:00',
+          date_start: '2020-12-25',
+          date_end: '2020-12-31',
+          room: { id: newRoom.id },
+        };
 
-      const sessionData = {
-        timetable: '14:00:00',
-        date_start: '2020-12-25',
-        date_end: '2020-12-31',
-        room: { id: newRoom.id },
-      };
-
-      const newSession = sessionRepository.create(sessionData);
-      await sessionRepository.save(newSession);
+        const newSession = sessionRepository.create(sessionData);
+        await sessionRepository.save(newSession);
+      }
     }
   }
 }
